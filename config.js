8ff218b5773e4c0a8e45b8e0834bdada3dc36e50
Aryan <prabhakararyan2007@gmail.com>
2025-03-16 00:53:12 +0530

    Initial commit

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 const firebaseConfig = {
     apiKey: "AIzaSyClIhXAaTVmlqhEPxU49C9w9fDkUag-1eQ",
     authDomain: "vibe-tunes.firebaseapp.com",
+    databaseURL: "https://vibe-tunes-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vibe-tunes",
+    storageBucket: "vibe-tunes.firebasestorage.app",
+    messagingSenderId: "792892627539",
+    appId: "1:792892627539:web:69817da08d2d4741a404a6",
+    measurementId: "G-7RQD85KP8Z"
 };
 firebase.initializeApp(firebaseConfig);
